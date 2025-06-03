8c943a3e71607c6154dbab2b82f3ac7a0cdc85ba
Nipun <nipunsingh29@hotmail.com>
2025-06-03 11:28:18 +0900

    Add fileupload

--- Week1/Streamlit/chapter04.py
+++ Week1/Streamlit/chapter04.py
@@ -12,3 +12,9 @@ if file:
     st.subheader(f"File: {file.name}")
     st.dataframe(df)
     st.write(f"The file size is {file.size / 1024:.2f} KB")
+
+if file:
+    cities = df["City"].unique()
+    selected_city = st.selectbox("Filter data by city", cities)
+    filtered_city = df[df["City"] == selected_city]
+    st.dataframe(filtered_city)
